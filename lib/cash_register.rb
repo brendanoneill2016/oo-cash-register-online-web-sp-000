f6c5095e9554de1aaa8f9341ccf2483721a4e8bd
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-12-09 12:53:37 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
 
+class CashRegister
+  attr_accessor :total, :discount
+  
+  
+  def initialize(discount=nil)
+    @total = 0
+    @discount = discount
+  end
+  
+  
+  def add_item(title,price,quantity=1)
+    previous_total = @total
+    self.total += price*quantity
+  end
+  
+  def apply_discount
+    
+    dis = sprintf('%.2f', (self.discount / 100) )
+
+
+
+
+    @total = dis 
+    
+  end
+  
+  
+  
+  
+end
